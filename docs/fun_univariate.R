8f16b24432247d3bb71a66c80ead093b15d0c57a
Henrik Singmann <singmann@gmail.com>
2020-05-14 20:37:07 +0200

    added overview of categorical variables

--- docs/fun_univariate.R
+++ docs/fun_univariate.R
@@ -1,4 +1,106 @@
 
+compare_categorical_covariate <- function(data, 
+                                          covariate, 
+                                          filter, 
+                                          plot = TRUE,
+                                          max_level_plot = 9,
+                                          rm_levels_less = 0.04,
+                                          alpha = 0.3) {
+  
+  if (!missing(filter)) {
+    prep_data_model <- fit_model_covariate(data = data, 
+                                           covariate = enquo(covariate), 
+                                           filter = enquo(filter))
+  } else {
+    prep_data_model <- fit_model_covariate(data = data,
+                                           covariate = enquo(covariate))
+  }
+  
+  props <- prep_data_model$data %>% 
+    group_by(!!enquo(covariate)) %>% 
+    tally() %>% 
+    mutate(prop = n / sum(n))
+  
+  if (nrow(props) > max_level_plot) {
+    levels_use <- props[[1]][props$prop >= rm_levels_less]
+  } else {
+    levels_use <- props[[1]]
+  }
+  
+  if (plot && length(levels_use) > 0 && length(levels_use) <= max_level_plot) {
+    new_xlab <- prep_data_model$varname
+    if (length(levels_use) < nrow(props)) {
+      new_xlab <- paste0(new_xlab, "; rem: ", 
+                         nrow(props) - length(levels_use), " levels")
+    }
+    pout <- data %>% 
+      dplyr::filter(!!sym(prep_data_model$varname) %in% levels_use) %>% 
+      ggplot(aes(x = !!sym(prep_data_model$varname), 
+                   y = abs_dev)) +
+      geom_violin() + 
+      stat_summary(fun.data = mean_se) +
+      facet_wrap("cond_x", nrow = 1) +
+    geom_label(
+      data = prep_data_model$dgof, 
+               aes(label = paste0("paste(italic(R) ^ 2, ' = ", 
+                    substr(
+                      formatC(r.squared, digits = 2, format = "f"), 
+                      2, 4), "')")), 
+              x = max(1.5, length(levels_use)-1.5), 
+              y = 0.9, parse = TRUE) +
+    coord_cartesian(ylim = c(0, 1)) +
+      labs(x = new_xlab, y = ylab) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    print(pout)
+  }
+  
+  df_out <- prep_data_model$dgof %>% 
+    select(cond_x, r.squared) %>% 
+    mutate(covariate = prep_data_model$varname, 
+           nlevels = nrow(props)) %>% 
+    pivot_wider(id_cols = c(covariate, nlevels), 
+                names_from = cond_x, 
+                values_from = r.squared)
+  return(df_out)
+  
+}
+
+fit_model_covariate <- function(data, 
+                                covariate, 
+                                filter) {
+  orig_n <- nrow(data)
+  if (!missing(filter)) {
+    data <- data %>% 
+      dplyr::filter( !!enquo(filter) )
+  }
+  
+  varname <- all.vars(covariate)
+  
+  data <- data %>% 
+    dplyr::filter(!is.na(!!sym(varname)))
+  
+  removed <- 1 - nrow(data)/orig_n
+
+  dlm <- data %>% 
+    group_by(cond_x) %>% 
+    nest() %>% 
+    mutate(lm = map(data, 
+                    ~lm(formula(expr(abs_dev ~ !!ensym(covariate))), .)))
+  dlm <- dlm %>% 
+    mutate(est = map(lm, ~ broom::tidy(.)), 
+           gof = map(lm, ~ broom::glance(.)))
+  dgof <- dlm %>% 
+    select(-lm, -est) %>% 
+    unnest(gof)
+  return(list(
+    data = data,
+    dlm = dlm, 
+    dgof = dgof, 
+    varname = varname, 
+    removed = removed
+  ))
+}
+
 compare_continuous_covariate <- function(data, 
                                          covariate, 
                                          filter,
