e43b8c8ae81d639a1bce10e40a480de08065e3de
Henrik Singmann <singmann@gmail.com>
2020-08-16 13:51:27 +0200

    first univariate plots for paper

--- analysis_paper.R
+++ analysis_paper.R
@@ -5,13 +5,16 @@ library("tidyverse")
 theme_set(theme_bw(base_size = 15) + 
             theme(legend.position="bottom", 
                   panel.grid.major.x = element_blank()))
+source("fun_analysis_paper.R")
+INCLUDE_GAM <- TRUE
 
 load("all_pairs_core.RData")
 all_pairs <- all_pairs %>% 
   mutate(rel_weight = (rel_par_weight_x + rel_par_weight_y)/2,
          rel_n = (rel_n_x + rel_n_y) / 2) %>% 
   mutate(se_x_w = if_else(se_x > 0.25, 0.25, se_x),
-         se_y_w = if_else(se_y > 0.25, 0.25, se_y)) %>% 
+         se_y_w = if_else(se_y > 0.25, 0.25, se_y),
+         rel_n_w = if_else(rel_n > 15000, 15000, rel_n)) %>% 
     mutate(cond_x2 = cond_y,
          cond_y2 = cond_x)
 
@@ -52,15 +55,102 @@ ggsave("figures_man/pairsplot_all.png", plot = ppairs,
        width = 29, height = 28, units = "cm", 
        dpi = 500)
 
+
+##----------------------------------------------------------------
+##                    Univariate Relationships                   -
+##----------------------------------------------------------------
+
+theme_set(theme_bw(base_size = 13) + 
+            theme(legend.position="bottom", 
+                  panel.grid.major.x = element_blank()))
+
+sel_methods <- c("Comp MLE", "No asy", "Beta PP", "Trait PP")
+#, "Trait_u PP"
+
+## see corresponding Report in docs folder for full overview
+str(all_pairs)
+targ_cmle <- all_pairs %>% 
+  filter(cond_y == "Comp MLE") %>% 
+  filter(cond_x != "Comp MLE")
+targ_lpp <- all_pairs %>% 
+  filter(cond_y == "Trait PP") %>% 
+  filter(cond_x != "Trait PP")
+
+targ_both <- bind_rows(targ_cmle, targ_lpp) %>% 
+  filter(cond_x %in% sel_methods) %>% 
+  droplevels %>% 
+  mutate(cond_label = factor(paste("DV:", cond_y), 
+                             levels = c("DV: Comp MLE", "DV: Trait PP"))) %>% 
+  mutate(cond_iv_label = factor(paste("IV:", cond_x), levels = 
+                                  paste(paste("IV:", levels(cond_x)))))
+  
+ylab <- "Abs. deviation"
+
+
+### parameter-level covariates
+pest <- compare_continuous_covariate(data = targ_both, covariate = poly(y, 2), 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Estimate (quadratic)")
+psex <- compare_continuous_covariate(data = targ_both, covariate = se_x_w, 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("SE (IV)")
+psey <- compare_continuous_covariate(data = targ_both, covariate = se_y_w, 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("SE (DV)")
+psd <- compare_continuous_covariate(data = targ_both, covariate = sd_emp_inv, 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Individual variability (SD)")
+prho <- compare_continuous_covariate(data = targ_both, covariate = rho_med, 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Parameter correlations (median)")
+pfungi <- compare_continuous_covariate(data = targ_both, covariate = sd_emp_inv, 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Parameter trade-off (max)")
+prelw <- compare_continuous_covariate(data = targ_both, covariate = log(rel_weight), 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Relative information (log)")
+preln <- compare_continuous_covariate(data = targ_both, covariate = log(rel_n_w), 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Relative N (log)")
+
+## data-set-level covariates
+phetero <- compare_continuous_covariate(data = targ_both, covariate = log1p_hetero, 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Hetereogeneity (log p + 1)")
+pfit <- compare_continuous_covariate(data = targ_both, covariate = log1p_fit_x, 
+                                     cond_label, cond_iv_label, ylab = ylab) +
+  xlab("Model fit (IV, log p + 1)")
+
+
+puniv_good <- cowplot::plot_grid(
+  psey, 
+  psex + theme(strip.text = element_blank()), 
+  pfungi + theme(strip.text = element_blank()), 
+  prelw + theme(strip.text = element_blank()), 
+  preln + theme(strip.text = element_blank()), 
+  ncol = 1, rel_heights = c(1.3, rep(1, 4)))
+ggsave("figures_man/univariate_good.png", plot = puniv_good, 
+       width = 28, height = 25, units = "cm", 
+       dpi = 500)
+
+puniv_notgood <- cowplot::plot_grid(
+  pest, 
+  psd + theme(strip.text = element_blank()), 
+  prho + theme(strip.text = element_blank()), 
+  phetero + theme(strip.text = element_blank()), 
+  pfit + theme(strip.text = element_blank()), 
+  ncol = 1, rel_heights = c(1.3, rep(1, 4)))
+ggsave("figures_man/univariate_notgood.png", plot = puniv_notgood, 
+       width = 28, height = 25, units = "cm", 
+       dpi = 500)
+
+
 ##----------------------------------------------------------------
 ##              Correlation of Relevant Covariates               -
 ##----------------------------------------------------------------
 
 
-str(all_pairs)
-targ_cmle <- all_pairs %>% 
-  filter(cond_y == "Comp MLE") %>% 
-  filter(cond_x != "Comp MLE")
+
 
 rel_vars <- c("se_x_w", "se_y_w", "fungi_max", "rel_weight", "rel_n")
 
---
+++ fun_analysis_paper.R
@@ -0,0 +1,105 @@
+
+compare_continuous_covariate <- function(data, 
+                                         covariate, 
+                                         ...,
+                                         filter,
+                                         ylab,
+                                         alpha = 0.3) {
+  orig_n <- nrow(data)
+  if (!missing(filter)) {
+    data <- data %>% 
+      dplyr::filter( !!enquo(filter) )
+  }
+  
+  varname <- all.vars(enquo(covariate))
+  
+  data <- data %>% 
+    dplyr::filter(!is.na(!!sym(varname)))
+  
+  removed <- 1 - nrow(data)/orig_n
+
+  dlm <- data %>% 
+    group_by(...) %>% 
+    summarise(lm = list(lm(formula(expr(abs_dev ~ !!enexpr(covariate)))))) 
+  dlm <- dlm %>% 
+    mutate(est = map(lm, ~ broom::tidy(.)), 
+           gof = map(lm, ~ broom::glance(.)))
+  dgof <- dlm %>% 
+    select(-lm, -est) %>% 
+    unnest(gof)
+  
+  rx <- range(data[[varname]], na.rm = TRUE)
+  
+  # rx <- data %>% 
+  #   summarise(range = list(range(!!enquo(covariate)))) %>% 
+  #   {.$range[[1]]}
+  # data <- data %>% 
+  #   mutate(new_x = as.vector(!!enquo(covariate)))
+  
+  varname <- all.vars(enquo(covariate))
+  enquo_cov <- enquo(covariate)
+  expr_cov <- rlang::get_expr(enquo_cov)
+  if (is.call(expr_cov)) {
+    expr_cov[[which(map_chr(rlang::call_args(expr_cov), as_label) == varname) + 
+                1]] <- quote(x)
+  } else {
+    expr_cov <- quote(x)
+  }
+  
+  new_xlab <- if (varname == as_label(enquo(covariate))) {
+    varname
+  } else {
+    paste0(varname, " (", as_label(enquo(covariate)), ")")
+  }
+  if (!missing(filter)) {
+    new_xlab <- paste0(new_xlab, "; filter: ", as_label(enquo(filter)))
+  }
+  if (removed > 0) {
+    new_xlab <- paste0(new_xlab, "; rem: ", 
+                       scales::percent(removed, accuracy = 0.1))
+  }
+  
+  outp <- ggplot(data, aes(x = !!sym(varname), 
+                   y = abs_dev)) +
+    geom_point(alpha = alpha)
+  if (INCLUDE_GAM) {
+    gamr2 <- function(gam){
+      1-((sum(residuals(gam)^2))/
+                 (sum((gam$y - mean(gam$y))^2)))
+    }
+    dgam <- data %>% 
+      group_by(...) %>% 
+      summarise(lm = list(mgcv::gam(formula(expr(
+        abs_dev ~ s(!!enexpr(covariate), bs = "ts")))))) 
+    dgam <- dgam %>% 
+      mutate(r.squared = map_dbl(lm, gamr2),
+             est = map(lm, ~ broom::tidy(.)), 
+             gof = map(lm, ~ broom::glance(.)))
+    outp <- outp + geom_smooth(method = "gam", se = FALSE, 
+                formula = y ~ s(x, bs = "ts"), colour = "red")
+  }
+  outp <- outp + 
+    geom_smooth(method = "lm", se = FALSE, 
+                formula = formula(expr(y ~ !!expr_cov)))  +
+    facet_wrap(vars(...), nrow = 1) +
+    geom_label(data = dgof, aes(label = paste0("paste(italic(R) ^ 2, ' = ", 
+                    substr(
+                      formatC(r.squared, digits = 2, format = "f"), 
+                      2, 4), "')")), 
+              x = rx[2] - 0.5*diff(rx), 
+              y = 0.95, parse = TRUE, colour = "blue")
+  if (INCLUDE_GAM) {
+    outp <- outp + 
+      geom_label(data = dgam, aes(label = paste0(#"paste(italic(R) ^ 2, ' = ", 
+                                                 substr(
+                      formatC(r.squared, digits = 2, format = "f"), 
+                      2, 4))),#"')")), 
+              x = rx[2] - 0.12*diff(rx), 
+              y = 0.95, parse = FALSE, colour = "red")
+  }
+  
+  outp +
+    coord_cartesian(ylim = c(0, 1)) +
+    labs(x = new_xlab, y = ylab)
+}
+
